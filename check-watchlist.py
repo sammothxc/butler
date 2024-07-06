09df34e33650c993b818718da5b4049db6a0f6e4
sammothxc <sdwarrior8@gmail.com>
2024-07-05 19:59:35 -0600

    update

--- check-watchlist.py
+++ check-watchlist.py
@@ -9,7 +9,6 @@ from selenium.webdriver.support import expected_conditions as EC
 def log_in(USERNAME, PASSWORD, driver):
     driver.get("https://www.instagram.com/accounts/login")
     
-    # input name & password
     driver.find_element(By.NAME, 'username').send_keys(USERNAME)
     driver.find_element(By.NAME, 'password').send_keys(PASSWORD)
 
@@ -17,20 +16,16 @@ def log_in(USERNAME, PASSWORD, driver):
         '/html/body/div[2]/div/div/div[2]/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[3]/button'))).click()
 
 def get_json(username, driver):
-    # get the text from site
     url = "https://www.instagram.com/web/search/topsearch/?query=" + username
     driver.get(url)
     text = driver.page_source
 
-    # strip it from bullshit
     if "{" not in text or "users" not in text:
         return {}
 
-    # cut off html part
     text = text[text.index("{"):]
     text = text[:text.index("<")]
 
-    # convert it to python and return
     try:
         return json.loads(text)
     except:
@@ -62,29 +57,24 @@ def check_username(driver, username, potential_alts):
 
 def checkprivateaccounts():
     with open("watchlist.txt", "r", encoding="utf-8") as wanted:
-        usernames_to_check = wanted.read().split(";")# WANTED usernames
+        usernames_to_check = wanted.read().split(";")
     
     with open("pass.txt", "r") as pass_file:
         creds = pass_file.read().split(";")
 
-    MAIN_USERNAME = creds[0] # YOUR username
+    MAIN_USERNAME = creds[0]
     MAIN_PASSWORD = creds[1]
-
 
     potential_alts = set()
     exists = {}
 
-    # open browser & log in Instagram
     driver = webdriver.Chrome()
     driver.implicitly_wait(10)
     log_in(MAIN_USERNAME, MAIN_PASSWORD, driver)
     time.sleep(5)
 
-    # iterate through the wanted usernames
     for username in usernames_to_check:
         exists[username] = check_username(driver, username, potential_alts)
         time.sleep(1)
     
     return exists
-    return "\n".join([f"{key}: {value}" for key, value in exists.items()])
-#==============END OF PLAGARIZED CODE LMAO ===============
