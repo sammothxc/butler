3e382656febd65615c8449cab37454b449fdb7ce
hahahihihi12345 <hahahihihi12345@gmail.com>
2024-07-06 16:41:20 +0200

    Update watchlist.py
    added the status_check function and updated the check_watchlist_function to use it instead of the slower check_username

--- watchlist.py
+++ watchlist.py
@@ -9,14 +9,36 @@ from selenium.webdriver.support import expected_conditions as EC
 watchlist_file = "/home/butler/butler/watchlist.txt"
 creds_file = "/home/butler/butler/creds.txt"
 
-def log_in(USERNAME, PASSWORD, driver):
+def log_in(USERNAME: str, PASSWORD: str, driver) -> None:
     driver.get("https://www.instagram.com/accounts/login")
     driver.find_element(By.NAME, 'username').send_keys(USERNAME)
     driver.find_element(By.NAME, 'password').send_keys(PASSWORD)
     WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, \
         '/html/body/div[2]/div/div/div[2]/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[3]/button'))).click()
 
-def get_json(username, driver):
+def status_check(username: str, driver) -> list[bool, str, bool]:
+    '''
+    checks if account exists, its id and if its private (returned in this order)
+    '''
+    # get the text from site
+    url = "https://www.instagram.com/web/search/topsearch/?query=" + username
+    driver.get(url)
+    text = driver.page_source
+    res = [False, "", False]
+    text = text[text.index("{"):]
+
+    first_name_index = text.index('"username":"') + len('"username":"')
+    first_id_index = text.index('"pk_id":"') + len('"pk_id":"')
+    first_private_index = text.index('"is_private":') + len('"is_private":')
+    
+    res[0] = text[first_name_index:first_name_index + len(username)] == username # is the first match our username?
+    if res[0]:
+        res[1] = text[first_id_index:text.index('"', first_id_index)]
+        res[2] = not text[first_private_index:text.index('"', first_private_index)] == "false"
+
+    return res
+
+def get_json(username, driver): # no longer used
     url = "https://www.instagram.com/web/search/topsearch/?query=" + username
     driver.get(url)
     text = driver.page_source
@@ -29,13 +51,14 @@ def get_json(username, driver):
     except:
         return {}
 
-def encode(string):
-    return "".join([char for char in string if char.isnumeric()])
+def encode(string: str) -> str: # no longer used
+    # I fucked this one up a bit in the original, isalpha instead of isnumeric
+    return "".join(char for char in string if char.isalpha())
 
-def get_most_likely(usernames, alts):
-    return [alt for alt in list(alts) if encode(alt) in set([encode(name) for name in usernames])]
+def get_most_likely(usernames, alts) -> list[str]: # no longer used
+    return [alt for alt in list(alts) if encode(alt) in {encode(name) for name in usernames}]
 
-def check_username(driver, username, potential_alts):
+def check_username(driver, username, potential_alts): # no longer used
     users_json = get_json(username, driver=driver)
     if users_json == {}:
         print("invalid JSON")
@@ -49,7 +72,44 @@ def check_username(driver, username, potential_alts):
             exists = (True, user['user']['pk_id'])
     return exists
 
-def check_watchlist_function():
+def check_watchlist_function() -> dict[str: list[bool, str, bool]]:
+    '''
+    checks usernames from watchlist_file
+    returns a dict with usernames as keys and a list of exists:bool, id:str, private:bool as values
+    expects watchlist file with a trailing newline
+    expects creds file with a username,password format
+    '''
+
+    # get creds
+    with open(creds_file, "r") as pass_file:
+        creds = pass_file.read().split(",")
+    MAIN_USERNAME = creds[0]
+    MAIN_PASSWORD = creds[1]
+
+    # create driver and log in
+    driver = webdriver.Firefox()
+    driver.implicitly_wait(10)
+    log_in(MAIN_USERNAME, MAIN_PASSWORD, driver)
+
+    exists = {}
+
+    # iter through the lines, strip them from newlines and run the check on them
+    with open(watchlist_file, "r", encoding="utf-8") as wanted:
+        exists = {username: status_check(username, driver) for username in map(str.strip, wanted)}
+    
+    return exists
+
+    # same thing without list comprehension:
+    with open(watchlist_file, "r", encoding="utf-8") as wanted:
+        for username in wanted:
+            username = username.strip()
+            exists[username] = status_check(username, driver)
+
+    return exists
+
+
+    # original code:
+
     with open(watchlist_file, "r", encoding="utf-8") as wanted:
         usernames_to_check = wanted.read().split("\n")
     with open(creds_file, "r") as pass_file:
