b6c33a64db0bf3dfaf62ae21f22351b215b06562
sammothxc <sdwarrior8@gmail.com>
2024-07-09 17:05:05 -0600

    update

--- watchlist.py
+++ watchlist.py
@@ -22,7 +22,8 @@ def log_in(USERNAME: str, PASSWORD: str, driver) -> None:
     driver.find_element(By.NAME, 'password').send_keys(PASSWORD)
     WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH,
         '/html/body/div[2]/div/div/div[2]/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[3]/button'))).click()
-    time.sleep(5)
+    time.sleep(6)
+    ## implement a check for the login
 
 
 def status_check(username: str, driver) -> list[bool, str, bool]:
