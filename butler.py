e33e6adcbeece29087bf84fb9542c63f61a14a18
sammothxc <sdwarrior8@gmail.com>
2024-07-09 15:56:43 -0600

    update

--- butler.py
+++ butler.py
@@ -26,13 +26,8 @@ intents.guilds = True
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready and online!")
-    parser = argparse.ArgumentParser()
-    parser.add_argument('updated', nargs='?', type=str, default=None)
-    args = parser.parse_args()
-    print(args.updated)
-    if args.updated == "updated":
-        channel = bot.get_channel(1257840623596208309)
-        await channel.send(f"_ _\n:white_check_mark: Butler updated to {os.getenv('BOT_VERSION')}.")
+    channel = bot.get_channel(1257840623596208309)
+    await channel.send(f"_ _\n:white_check_mark: Butler updated to {os.getenv('BOT_VERSION')}.")
 
 
 def get_updates(repo_path):
