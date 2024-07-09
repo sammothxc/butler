b1730c39e79e47aee7bdbf9bac2ab826bd5439df
sammothxc <sdwarrior8@gmail.com>
2024-07-09 17:15:49 -0600

    update

--- butler.py
+++ butler.py
@@ -111,8 +111,7 @@ async def list_watchlist(ctx: discord.ApplicationContext):
         await ctx.respond("_ _\n:x: Watchlist error.")
         return
     await ctx.respond(f"_ _\n:white_check_mark: Watchlist accounts:\n{watchlist}")
-    message = await ctx.fetch_message(ctx.message.id)
-    await message.edit(suppress=True)
+    await ctx.edit(suppress=True)
 
 
 # Check Watchlist
