f40e1ce3aed95b393cf11a9a997e5bf72414b43e
sammothxc <sdwarrior8@gmail.com>
2024-07-09 15:00:14 -0600

    update

--- butler.py
+++ butler.py
@@ -27,11 +27,11 @@ async def on_ready():
     parser = argparse.ArgumentParser()
     parser.add_argument('--flag', action='store_true')
     args = parser.parse_args()
+    print(args.flag)
     if args.flag:
         #  channel_id = os.getenv('DISCORD_CHANNEL') # doesn't work for some reason
         channel = bot.get_channel(1257840623596208309)
         print(channel)
-        print(args.flag)
         await channel.send(f"_ _\n:white_check_mark: Butler updated to {os.getenv('BOT_VERSION')}.")
 
 
